3a2fcd966940c06541fa4d1e83a5c1befa4540cf
Biswa388 <DASH6427@GMAIL.COM>
2025-12-02 02:15:29 +0530

    Update greeting message in HelloController

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloController {
     public String message() {
-        return "Hello from 1st DevOps Project!";
+        return "Hello from 2nd DevOps Project!";
     }
 }
